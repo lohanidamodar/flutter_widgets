87f4cf39535c12aad479cfc50fe6500e0fd77cdc
Damodar Lohani <dlohani48@gmail.com>
2019-04-18 19:33:38 +0545

    few more container properties

--- lib/container.dart
+++ lib/container.dart
@@ -7,27 +7,58 @@ class ContainerPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Container Widget'),
       ),
-      body: Container(
-        width: double.infinity,
-        height: 300,
-        alignment: Alignment.bottomRight,
-        foregroundDecoration: BoxDecoration(
-          color: Colors.red.withOpacity(0.5)
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              height: 300,
+              alignment: Alignment.bottomRight,
+              foregroundDecoration: BoxDecoration(
+                color: Colors.red.withOpacity(0.5)
+              ),
+              decoration: BoxDecoration(
+                color: Colors.green,
+                border: Border(left: BorderSide(
+                  width: 5.0,
+                  color: Colors.red,
+                  style: BorderStyle.solid
+                ))
+              ),
+              padding: EdgeInsets.all(20.0),
+              margin: EdgeInsets.all(20.0),
+              child: Text("Hello Container!",style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.white
+              ),)
+            ),
+            Container(
+              width: double.infinity,
+              height: 100,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.redAccent, Colors.red]
+                )
+              ),
+              padding: EdgeInsets.all(20.0),
+              margin: EdgeInsets.all(20.0),
+              child: Text("Container Gradient",style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white
+              ),)
+            ),
+            Container(
+              width: 200,
+              height: 100,
+              color: Colors.red,
+              transform: Matrix4.rotationZ(0.1),
+              child: Text("Container transform",style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white
+              ),)
+            ),
+          ],
         ),
-        decoration: BoxDecoration(
-          color: Colors.green,
-          border: Border(left: BorderSide(
-            width: 5.0,
-            color: Colors.red,
-            style: BorderStyle.solid
-          ))
-        ),
-        padding: EdgeInsets.all(20.0),
-        margin: EdgeInsets.all(20.0),
-        child: Text("Hello Container!",style: TextStyle(
-          fontSize: 30.0,
-          color: Colors.white
-        ),)
       ),
     );
   }
